c75dbef6c9b803bc49e15ae18171f331e62851b3
Markan123 <pesicmarko258@gmail.com>
2024-03-28 17:50:16 +0100

    Vise nije hello world nego H

--- app/src/main/java/com/example/zad2/MainActivity.java
+++ app/src/main/java/com/example/zad2/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+	int novaPromenljiva=0;
         setContentView(R.layout.activity_main);
     }
 }
